41baf3fdec4b269450ae7a9b626218d1e8642f3a
Charles Severance <csev@umich.edu>
2015-04-21 14:40:09 -0400

    Tweak the order of the glossary

--- audio.sh
+++ audio.sh
@@ -9,6 +9,6 @@ for f in [0-9]*.mkd
 do
  x=`basename $f .mkd`
  echo "Processing $f $x"
- pandoc $f -t plain | sed '/^Questions/,$d' | cat - audio.postamble | say -v Samantha -o audio/sam-$x.mp4 --file-format=mp4f
- pandoc $f -t plain | sed '/^Questions/,$d' | cat - audio.postamble | say -v Tom -o audio/tom-$x.mp4 --file-format=mp4f
+ pandoc $f -t plain | sed '/^Glossary/,$d' | cat - audio.postamble | say -v Samantha -o audio/sam-$x.mp4 --file-format=mp4f
+ pandoc $f -t plain | sed '/^Glossary/,$d' | cat - audio.postamble | say -v Tom -o audio/tom-$x.mp4 --file-format=mp4f
 done
