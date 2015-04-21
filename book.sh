c8c8a3dc3669d374df5754e5acee1c6390b5b1d2
Charles Severance <csev@umich.edu>
2015-04-21 12:31:05 -0400

    Add the glossary - tweak some figures.

--- book.sh
+++ book.sh
@@ -3,7 +3,7 @@
 rm tmp.* *.tmp *.aux
 pandoc A1-preface.mkd -o tmp.prefacex.tex
 sed < tmp.prefacex.tex 's/section{/section*{/' > tmp.preface.tex
-pandoc -s -N --toc --default-image-extension=eps -V fontsize:11pt -V documentclass:book --template=template.latex [0-9]*.mkd -o tmp.tex
+pandoc -s -N -f markdown+definition_lists --toc --default-image-extension=eps -V fontsize:11pt -V documentclass:book --template=template.latex [0-9]*.mkd -o tmp.tex
 
 latex tmp
 makeindex tmp
