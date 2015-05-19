102f8d55075881f96f6504f20c879d16aa4e18b9
Charles Severance <csev@umich.edu>
2015-05-18 21:14:31 -0400

    Added a photo of a telephone pole near I-57 in Illinois.

--- book/book.sh
+++ book/book.sh
@@ -1,10 +1,17 @@
 #! /bin/sh
 
+# For yucks make the epub
+pandoc --default-image-extension=svg --epub-stylesheet=epub.css *.mkd -o x.epub
+
 rm tmp.* *.tmp *.aux
 pandoc A1-preface.mkd -o tmp.prefacex.tex
 sed < tmp.prefacex.tex 's/section{/section*{/' > tmp.preface.tex
 pandoc -s -N -f markdown+definition_lists --toc --default-image-extension=eps -V fontfamily:arev -V fontsize:11pt -V documentclass:book --template=template.latex [0-9]*.mkd -o tmp.tex
 
+sed < tmp.tex '/includegraphics/s/jpg/eps/' | sed 's"includegraphics{../photos"includegraphics[height=3.0in]{../photos"' > tmp.sed
+diff tmp.sed tmp.tex
+
+mv tmp.sed tmp.tex
 latex tmp
 makeindex tmp
 latex tmp
