c31d21f19a7291f947ee60526663f1432b485e83
Charles Severance <csev@umich.edu>
2021-06-02 09:07:53 -0400

    Remove disqus

--- index.php
+++ index.php
@@ -76,21 +76,4 @@ Coursera.</p>
 
 <?php require_once('javascript.php'); ?>
 
-<hr/>
-<div id="disqus_thread"></div>
-<script type="text/javascript">
-    /* * * CONFIGURATION VARIABLES * * */
-    var disqus_shortname = 'net-intro';
-    var disqus_shortname = 'net-intro';
-    
-    /* * * DON'T EDIT BELOW THIS LINE * * */
-    (function() {
-        var dsq = document.createElement('script'); dsq.type = 'text/javascript'; dsq.async = true;
-        dsq.src = '//' + disqus_shortname + '.disqus.com/embed.js';
-        (document.getElementsByTagName('head')[0] || document.getElementsByTagName('body')[0]).appendChild(dsq);
-    })();
-</script>
-<noscript>Please enable JavaScript to view the <a href="https://disqus.com/?ref_noscript" rel="nofollow">comments powered by Disqus.</a></noscript>
-</div>
-
 </body>
