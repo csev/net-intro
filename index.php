5bb38b62d031af57386122762208f7bd4e1acb76
Charles Severance <csev@umich.edu>
2023-02-01 22:11:10 -0500

    Updates.

--- index.php
+++ index.php
@@ -29,7 +29,7 @@ that needs fixing.  Thanks in advance.</p>
              license and the print copy is CC-BY-NC.</p>
           <p><a class="btn btn-primary" href="http://do1.dr-chuck.net/net-intro/EN_us/net-intro.pdf" target="_blank">PDF &raquo;</a>
           <a class="btn btn-primary" href="http://do1.dr-chuck.net/net-intro/EN_us/net-intro.epub" target="_blank">EPUB &raquo;</a>
-          <a class="btn btn-primary" href="http://www-personal.umich.edu/~csev/books/net-intro/audio/" target="_blank">Audio &raquo;</a>
+          <a class="btn btn-primary" href="https://audio.net-intro.com" target="_blank">Audio &raquo;</a>
           <a class="btn btn-primary" style="margin-top:2px;" href="book/translations/Spanish/2021-06-12-book.pdf" target="_blank">Spanish &raquo;</a>
           <a class="btn btn-primary" style="margin-top:2px;" href="book/translations/Greek/2021-06-28-book.pdf" target="_blank">Greek &raquo;</a>
           <a class="btn btn-primary" style="margin-top:2px;" href="book/translations/Russian/2021-07-11-book.pdf" target="_blank">Russian &raquo;</a>
